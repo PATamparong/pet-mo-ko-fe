c3fafca53f363fc700bd8b8ebcd2fde595a3d656
PATamparong14 <tamparongpreciousangel14@gmail.com>
2023-03-29 10:30:14 +0800

    modify routes

--- src/routes/PublicRoutes/index.tsx
+++ src/routes/PublicRoutes/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Animals from "../../pages/Animals";
 import Dashboard from "../../pages/Dashboard";
+import AppServices from "../../pages/AppServices";
+import Details from "../../pages/Details";
 
 export default function PublicRoutes() {
   return (
@@ -9,6 +11,8 @@ export default function PublicRoutes() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/animals" element={<Animals />} />
+        <Route path="/services" element={<AppServices />} />
+        <Route path="/details" element={<Details />} />
       </Routes>
     </Router>
   );
