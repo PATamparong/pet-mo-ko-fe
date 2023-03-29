a2a89bb0f5e9bd38e0ad8ae2a36465fcb1604f80
PATamparong14 <tamparongpreciousangel14@gmail.com>
2023-03-29 10:30:07 +0800

    modify pages

---
+++ src/pages/AppServices/index.tsx
@@ -0,0 +1,11 @@
+import { Layout } from "../../components";
+import React from "react";
+import { Center } from "../../styles";
+
+export default function AppServices() {
+  return (
+    <Layout headerName="Pet Mo Ko">
+      <Center VH>services</Center>
+    </Layout>
+  );
+}
